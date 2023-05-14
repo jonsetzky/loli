86a76b4fb917bae14cb657679f8d0102370571c4
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2023-05-14 20:56:54 +0300

    many changes: should work in some ways

---
+++ src/components/RoleIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Tooltip } from "./Tooltip";
+
+export const RoleIcon = (
+  props: {
+    position: string;
+    invert?: boolean;
+    opacity?: number;
+  } & React.ImgHTMLAttributes<HTMLImageElement>
+) => {
+  const { position, invert, opacity, ...p } = props;
+  return (
+    <>
+      <Tooltip anchorSelect="">position</Tooltip>
+      <img
+        {...p}
+        src={`./positions/position-${position.toLowerCase()}.svg`}
+        style={{
+          filter: `brightness(${invert ? "1" : "0"}) grayscale(100%)`,
+          opacity: opacity,
+        }}
+      />
+    </>
+  );
+};
